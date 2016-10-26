6d092e03c32e8fc0fa93dbda8993da05cde809fd
Jonathan Leczner <jleczner@udel.edu>
2016-10-25 20:29:18 -0400

    initial commit

---
+++ src/main/java/leczner/jon/HR_App/HrAppApplication.java
@@ -0,0 +1,12 @@
+package leczner.jon.HR_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrAppApplication.class, args);
+	}
+}
---
+++ src/test/java/leczner/jon/HR_App/HrAppApplicationTests.java
@@ -0,0 +1,16 @@
+package leczner.jon.HR_App;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HrAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
